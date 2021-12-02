965111d428b432abce0e2707807a0bc78628ebc6
BianchiDev <ifpebianchi@gmail.com>
2021-12-02 12:23:56 -0300

    Iniciando o exercio 01 de Operadores Matemáticos em java

---
+++ Java-dio/exercicio01/exercicio01/src/exercicio01/aula01.java
@@ -0,0 +1,20 @@
+package exercicio01;
+
+public class aula01 {
+	public static void main(String[] args) {
+		
+		int i ;
+//		int i;
+//		int 1a;
+		int _1a;
+		int $aq;
+		
+		final int j = 10;
+		
+		int asrn246feíae;
+		int asrn246f_eía_e;
+//		int asrn246feíae = 10;
+//		int asrn246feíae = 10;
+		
+	}
+}
